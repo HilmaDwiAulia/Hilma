da4db8a0759f7ca56bd869ab5608ceee9fd12ab9
HilmaDwiAulia <127916113+HilmaDwiAulia@users.noreply.github.com>
2023-03-21 10:08:07 +0700

    first

---
+++ Komentas.js
@@ -0,0 +1,5 @@
+//consule.log("ini adalah komen baris satu");
+console.log("Tugas Komen //");
+
+/*console.log("ini adalah komen lebih dari satu baris");
+conlose.log("Teks apapun yang berada disini")
